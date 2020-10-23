8b9d0109f5584a0944987bc5280e4354e1f313d0
migsladisla <migsladisla@gmail.com>
2020-10-23 20:45:03 +0800

    Setup responsive navbar and section background colors

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+var prevScrollpos = window.pageYOffset;
+var navbar = document.getElementById("navbar");
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+  
+    if (currentScrollPos > 60) {
+        navbar.style.zIndex =  1;
+        navbar.style.opacity =  1;
+        navbar.classList.add("sticky", "reveal-in");
+    } else {
+        navbar.style.zIndex =  -1;
+        navbar.style.opacity =  0;
+        navbar.classList.remove("sticky", "reveal-in");
+    }
+}
+
+
+function expandNavbar() {
+    console.log(navbar.classList)
+    if (navbar.classList.length === 3) {
+        navbar.classList.add("responsive");
+    } else {
+        navbar.classList.remove("responsive");
+    }
+}
